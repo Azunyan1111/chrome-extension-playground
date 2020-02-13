da4d7c317a272b5086cb80c86987342d4c135971
Azunyan1111 <admin@mail.azunyan1111.com>
2020-02-13 17:32:04 +0900

    save

--- 00-hello-world-in-console/background.js
+++ 00-hello-world-in-console/background.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+
+
+// strip_invalid('#base58', base58.alphabet);
+
+b58 = document.getElementById("CentItemAdditional2").innerHTML.trim();
+console.log(b58);
+plain = base58.decode(b58);
+console.log(plain);
+asin = base58.string(plain);
+console.log(asin);
+console.log("https://amazon.co.jp/dp/" + asin);
+
+document.getElementById("CentItemAdditional2").innerHTML = "<a href='" + "https://amazon.co.jp/dp/" + asin + "'>アマゾンURL:https://amazon.co.jp/dp/" + asin + "<a/>";
